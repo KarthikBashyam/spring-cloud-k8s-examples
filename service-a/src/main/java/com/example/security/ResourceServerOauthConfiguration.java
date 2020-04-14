354b950e41685af3177bb3911837c19bbf60f6e3
karthikb4ps@gmail.com <karthikb4ps@gmail.com>
2020-04-14 15:06:59 -0400

    Made changes to pipeline

--- service-a/src/main/java/com/example/security/ResourceServerOauthConfiguration.java
+++ service-a/src/main/java/com/example/security/ResourceServerOauthConfiguration.java
@@ -1,6 +1,9 @@
 package com.example.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
+import org.springframework.boot.actuate.health.HealthEndpoint;
+import org.springframework.boot.actuate.info.InfoEndpoint;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -18,7 +21,10 @@ public class ResourceServerOauthConfiguration extends WebSecurityConfigurerAdapt
 	protected void configure(HttpSecurity http) throws Exception {		
 		 //@formatter:off
 			http.addFilterAfter(filter, SecurityContextPersistenceFilter.class)
-			     .authorizeRequests().mvcMatchers("/service-a**").access("hasAuthority('SCOPE_authority-a')")
+			     .authorizeRequests()
+			     .requestMatchers(EndpointRequest.to(InfoEndpoint.class, HealthEndpoint.class))
+			     .permitAll()
+			     .mvcMatchers("/service-a**").access("hasAuthority('SCOPE_authority-a')")
 			     .anyRequest()
 				 .authenticated()
 				 .and()
